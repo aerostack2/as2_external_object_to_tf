ddaf23f624e9b1b63ef005c6b4b5571b45cbb098
Javilinos <javier.mdeza@alumnos.upm.es>
2023-04-25 11:27:08 +0200

    add object in gps working

--- launch/as2_external_object_to_tf_launch.py
+++ launch/as2_external_object_to_tf_launch.py
@@ -11,7 +11,6 @@ def generate_launch_description():
             package='as2_external_object_to_tf',
             executable='as2_external_object_to_tf_node',
             name='as2_external_object_to_tf',
-            prefix=['xterm -e gdb -ex run --args'],
             namespace=LaunchConfiguration('namespace'),
             parameters=[{'use_sim_time': LaunchConfiguration('use_sim_time')},
                         {'config_file': LaunchConfiguration('config_file')}],
